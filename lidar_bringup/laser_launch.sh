290598f3a969a676a03ba699377493aa72afb2da
daftpwner <daftpwner@gmail.com>
2015-07-07 16:25:57 -0500

    Began cleaning lidar setup scripts.

--- lidar_bringup/laser_launch.sh
+++ lidar_bringup/laser_launch.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-sudo chmod a=r+w /dev/pts/*
-sudo chmod a=r+w /dev/ttyS51
-rosparam set hokuyo_node/port /dev/ttyS51
-rosrun hokuyo_node hokuyo_node
+chmod a=r+w /dev/pts/*
+chmod a=r+w /dev/ttyS51
--- lidar_bringup/laser_setup.sh
+++ lidar_bringup/laser_setup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-mknod -m 666 /dev/ttyS51 c 4 115
-socat PTY,link=/dev/ttyS51, TCP4:192.168.0.70:10002
-
+if [!-f "/dev/ttyS51"]
+then
+	mknod -m 666 /dev/ttyS51 c 4 115
+	socat PTY,link=/dev/ttyS51, TCP4:192.168.0.70:10002
+fi
